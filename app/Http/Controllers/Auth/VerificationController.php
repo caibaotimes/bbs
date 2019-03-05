1547f87a9b91a97b57d111ac96cba471f88f136c
vagrant <vagrant@homestead>
2019-03-05 02:50:55 +0000

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -34,8 +34,12 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        //设定了所有的控制器动作都需要登录后才能访问
         $this->middleware('auth');
+        //只有verify动作使用signed 中间件进行认证，signed中间件是一种由框架提供的很方便的url前面认证方式
         $this->middleware('signed')->only('verify');
+        //对verify和resend动作做了频率限制，throttle中间件接收两个参数，这两个参数决定了在给定的分钟数内
+        //可以进行的最大请求数
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
