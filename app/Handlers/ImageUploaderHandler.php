4fb53d7c38a6626bdf338b27c20e9581134b1f15
vagrant <vagrant@homestead>
2019-03-06 07:15:15 +0000

    裁剪头像

--- app/Handlers/ImageUploaderHandler.php
+++ app/Handlers/ImageUploaderHandler.php
@@ -7,12 +7,12 @@
  */
 
 namespace App\Handlers;
-
+use Image;
 
 class ImageUploaderHandler
 {
-    protected $allowed_ext = ['jpeg','bmp','png','gif'];
-    public function save($file,$folder,$file_prefix,$width=false)
+    protected $allowed_ext = ['jpeg','jpg','png','gif'];
+    public function save($file,$folder,$file_prefix,$max_width=false)
     {
         //定义保存图片的文件夹路径
         $folder_name = "uploads/images/$folder/".date("Y/md",time());
@@ -28,10 +28,30 @@ class ImageUploaderHandler
         }
         //将图片移动到我们的目标存储路径中
         $file->move($upload_path,$filename);
+        //如果限制了图片宽度，就进行裁剪
+        if($max_width && $extension != 'gif'){
+            //裁切图片
+            $this->reduceSize($upload_path.'/'.$filename,$max_width);
+        }
 
         return [
             'path'=>config('app_url')."/$folder_name/$filename"
         ];
+    }
+    
+    public function reduceSize($file_path,$max_width)
+    {
+        //先实例化，传参为文件的磁盘物理路径
+        $image = Image::make($file_path);
+        //进行大小调整的操作
+        $image->resize($max_width,null,function ($constraint){
+            //设定宽度是$max_width,高度等比缩放
+            $constraint->aspectRatio();
+            //防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+        //对图片修改后进行保存
+        $image->save();
     }
 }
 
