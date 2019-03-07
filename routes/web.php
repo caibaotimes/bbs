c3676fb54f4cdd501f22ae0160b88ef929d66dd7
vagrant <vagrant@homestead>
2019-03-07 02:57:15 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 
 Route::get('/','PagesController@root')->name('root');
 
-Auth::routes();
+//Auth::routes();
 
 //用户身份验证相关的路由
 Route::get('login','Auth\LoginController@showLoginForm')->name('login');
@@ -37,44 +37,4 @@ Route::get('email/verify/{id}','Auth\VerificationController@verify')->name('veri
 Route::get('email/resend','Auth\VerificationController@resend')->name('verification.resend');
 
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
