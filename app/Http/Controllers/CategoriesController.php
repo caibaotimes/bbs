11263704f19cfc197a818353d7876cca496d3330
vagrant <vagrant@homestead>
2019-03-08 02:05:34 +0000

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,10 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category,Topic $topic,Request $request)
     {
         //读取分类ID关联的话题，并按每20天分页
-        $topics = Topic::where('category_id',$category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
         //传参变量话题和分类到模板中
         return view('topics.index',compact('topics','category'));
     }
