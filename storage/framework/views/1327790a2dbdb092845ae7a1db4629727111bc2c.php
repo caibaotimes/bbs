2b12011e09edf1b82f9ca5f2f720e91564423bb0
caibaotimes <tutucheyouhui@sina.com>
2019-03-24 05:01:34 +0000

    活跃用户

--- storage/framework/views/1327790a2dbdb092845ae7a1db4629727111bc2c.php
+++ storage/framework/views/1327790a2dbdb092845ae7a1db4629727111bc2c.php
@@ -5,3 +5,22 @@
         </a>
     </div>
 </div>
+
+<?php if(count($active_users)): ?>
+  <div class="card mt-4">
+    <div class="card-body active-users pt-2">
+      <div class="text-center mt-1 mb-0 text-muted">活跃用户</div>
+      <hr class="mt-2">
+      <?php $__currentLoopData = $active_users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $active_user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <a class="media mt-2" href="<?php echo e(route('users.show', $active_user->id)); ?>">
+          <div class="media-left media-middle mr-2 ml-1">
+            <img src="<?php echo e($active_user->avatar); ?>" width="24px" height="24px" class="media-object">
+          </div>
+          <div class="media-body">
+            <small class="media-heading text-secondary"><?php echo e($active_user->name); ?></small>
+          </div>
+        </a>
+      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </div>
+  </div>
+<?php endif; ?>
